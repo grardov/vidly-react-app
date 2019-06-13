a9c3b9bc9b262dbbe99a33ff0dbe4c3aa4e32d4c
Gerardo Valencia <gerardo@yellowme.mx>
2019-06-13 11:10:04 -0500

    Added validation to display delete button. You only delete a movie when you're a admin and logged.

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,36 +1,44 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import auth from "../services/authService";
 import Table from "./common/table";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
   columns = [
     {
-      path: 'title',
-      label: 'Title',
+      path: "title",
+      label: "Title",
       content: movie => <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
     },
-    { path: 'genre.name', label: 'Genre' },
-    { path: 'numberInStock', label: 'Stock' },
-    { path: 'dailyRentalRate', label: 'Rate' },
-    { 
-      key: 'like', 
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
       content: movie => (
         <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
       )
-    },
-    {
-      key: 'delete', 
-      content: movie => (
-        <button
-          onClick={() => this.props.onDelete(movie)}
-          className="btn btn-danger btn-sm"
-        >
-        Delete
-        </button>
-      )
     }
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: movie => (
+      <button
+        onClick={() => this.props.onDelete(movie)}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    )
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAuth) this.columns.push(this.deleteColumn);
+  }
 
   render() {
     const { movies, onSort, sortColumn } = this.props;
